3794b4c1792dd68f358473bca502934233afeef2
vieirads <dennersvieira@gmail.com>
2020-08-14 11:49:52 -0300

    Create the buttons: 'Done', 'Skip', 'Delete', 'Start', and 'Pause'. Added simple functionalities to them. 'Done' cross the current item and display its status, 'Skip' cross the current item set its status and zero the time, 'Delete' delete the task from the list, 'Start' and 'Pause' just modify the status but the latter also change the label of the button to 'Resume', so you can go back to the actvities.

--- script.js
+++ script.js
@@ -1,36 +1,69 @@
 function TodoListController($scope) {
-    $scope.todos = [
-        {chore: 'Create this app', time: 1, done: false},
-        {chore: 'Make it work properly', time: 1, done: false},
-        {chore: 'Put time in hours:minutes:seconds', time: 2, done: false},
-        {chore: 'Make the delete button works', time: 2, done: false},
-        {chore: 'Make the coundown of the current chore', time: 3, done: false}
+    $scope.todoList = [
+        {status: 'Queued', task: 'Create this app', time: 1, done: false},
+        {status: 'Queued', task: 'Make it work properly', time: 1, done: false},
+        {status: 'Queued', task: 'Put time in hours:minutes:seconds', time: 2, done: false},
+        {status: 'Queued', task: 'Make the coundown of the current task', time: 3, done: false},
+        {status: 'Queued', task: "Make the 'Start' button works", time: 2, done: false},
+        {status: 'Queued', task: "Make the 'Pause' button works", time: 2, done: false},
+        {status: 'Done', task: "Make the 'Done' button works", time: 2, done: true},
+        {status: 'Done', task: "Make the 'Skip' button works", time: 2, done: true},
+        {status: 'Done', task: "Make the 'Delete' button works", time: 2, done: true},
     ];
 
-    $scope.addChore = function() {
-        $scope.todos.push({
-            chore: $scope.todo.chore,
+    $scope.addTask = function() {
+        $scope.todoList.push({
+            task: $scope.todo.task,
             time: $scope.todo.time,
             done: false
         });
-        $scope.todo.chore = $scope.todo.time = '';
+        $scope.todo.task = $scope.todo.time = '';
     };
 
-    $scope.delChore = function() {
-        
+    $scope.startTasks = function() {
+        $scope.todoList[0].status = 'In progress';
+        // add countown
     }
+
+    $scope.pauseResume = 'Pause';
+    $scope.pauseTasks = function() {
+        if ($scope.pauseResume === 'Pause') {
+            $scope.todoList[0].status = 'Paused';
+            $scope.pauseResume = 'Resume';
+        } else {
+            $scope.todoList[0].status = 'In progress';
+            $scope.pauseResume = 'Pause';
+        }
+        // add pauseResume countdown
+    }
+
+    $scope.doneTask = function(index) {
+        $scope.todoList[index].done = true;
+        $scope.todoList[index].status = 'Done';
+    }
+
+    $scope.skipTask = function(index) {
+        $scope.todoList[index].done = true;
+        $scope.todoList[index].time = 0;
+        $scope.todoList[index].status = 'Skipped';
+    }
+
+    $scope.delTask = function(index) {
+        $scope.todoList.splice(index, 1);
+    }
+
 
     $scope.totalTime = function() {
         let count = 0;
-        for (todo of $scope.todos) {
+        for (todo of $scope.todoList) {
             count += todo.time;
         }
         return count;
     }
 
-    $scope.completedChores = function() {
+    $scope.completedTasks = function() {
         let count = 0;
-        for (todo of $scope.todos) {
+        for (todo of $scope.todoList) {
             count += todo.done ? 1 : 0;
         };
         return count;
@@ -38,7 +71,7 @@ function TodoListController($scope) {
 
     $scope.ellapsedTime = function() {
         let count = 0;
-        for (todo of $scope.todos) {
+        for (todo of $scope.todoList) {
             if (todo.done) {
                 count += todo.time;
             }
