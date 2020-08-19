57a868e61b637d82964fd66c14c65bd50e8e9c21
vieirads <dennersvieira@gmail.com>
2020-08-19 14:16:08 -0300

    Modified the hour input and added a new property to the skip button, which has a bug in it. Have to fix this.

--- script.js
+++ script.js
@@ -1,29 +1,42 @@
 var taskcounter = angular.module('taskcounter', []);
 taskcounter.controller("TodoListController", function($scope, $interval) {
     $scope.todoList = [
-        {status: 'Queued', task: 'Create this app', time: '1:20:00', done: false},
-        {status: 'Queued', task: 'Make it work properly', time: '1:30:00', done: false},
-        {status: 'Queued', task: 'Make the coundown of the current task', time: '1:00:00', done: false},
-        {status: 'Queued', task: "Make the 'Start' button works", time: '1:00:00', done: false},
-        {status: 'Queued', task: "Make the 'Pause' button works", time: '1:00:00', done: false},
-        {status: 'Done', task: 'Put time in hours:minutes:seconds', time: '1:40:00', done: true},
-        {status: 'Done', task: "Make the 'Done' button works", time: '1:25:00', done: true},
-        {status: 'Done', task: "Make the 'Skip' button works", time: '0:30:00', done: true},
-        {status: 'Done', task: "Make the 'Delete' button works", time: '1:40:00', done: true},
+        {status: 'Queued', task: 'Create this app', time: '01:20:00', done: false},
+        {status: 'Queued', task: 'Make it work properly', time: '01:30:00', done: false},
+        {status: 'Queued', task: 'Make the coundown of the current task', time: '01:00:00', done: false},
+        {status: 'Queued', task: "Make the 'Start' button works", time: '01:00:00', done: false},
+        {status: 'Queued', task: "Make the 'Pause' button works", time: '01:00:00', done: false},
+        {status: 'Done', task: 'Put time in hours:minutes:seconds', time: '01:40:00', done: true},
+        {status: 'Done', task: "Make the 'Done' button works", time: '01:25:00', done: true},
+        {status: 'Done', task: "Make the 'Skip' button works", time: '00:30:00', done: true},
+        {status: 'Done', task: "Make the 'Delete' button works", time: '01:40:00', done: true},
     ];
 
+    $scope.hours   = [...Array(24).keys()];
+    $scope.minutes = [...Array(12).keys()].map((x) => x*5);
+
     $scope.addTask = function() {
+
+        let setTime = new Date();
+        setTime.setHours($scope.hour);
+        setTime.setMinutes($scope.minute);
+        setTime.setSeconds(0);
+
         $scope.todoList.push({
             status: 'Queued',
             task: $scope.todo.task,
-            time: $scope.todo.time,
+            time: setTime,
             done: false
         });
+
         $scope.todo.task = $scope.todo.time = '';
+        $scope.hour = $scope.minute = '';
     };
 
+    
     $scope.startTasks = function() {
         $scope.todoList[0].status = 'In progress';
+        // has to stop the final time clock
     }  
     
 
@@ -46,10 +59,20 @@ taskcounter.controller("TodoListController", function($scope, $interval) {
         // $scope.todoList.push(todoList.splice(index, 1));
     }
 
+    $scope.skipRetake = 'Skip';
     $scope.skipTask = function(index) {
-        $scope.todoList[index].done = true;
-        $scope.todoList[index].time = 0;
-        $scope.todoList[index].status = 'Skipped';
+        $scope.skipRetake = 'Skip';
+        if ($scope.skipRetake === 'Skip') {
+            $scope.todoList[index].done = true;
+            $scope.todoList[index].status = 'Skipped';
+            $scope.skipRetake = 'Retake';
+        } else {
+            $scope.todoList[index].done = false;
+            $scope.todoList[index].status = 'Queued';
+            $scope.skipRetake = 'Skip';
+        }
+        // $scope.todoList[index].done = true;
+        // $scope.todoList[index].status = 'Skipped';
     }
 
     $scope.delTask = function(index) {
@@ -61,10 +84,17 @@ taskcounter.controller("TodoListController", function($scope, $interval) {
         let totalMinutes = 0;
         let totalSeconds = 0;
         for (todo of $scope.todoList) {
-            let todoTime = todo.time.split(':').map((x) => +x);
-            totalHours   += todoTime[0];
-            totalMinutes += todoTime[1];
-            totalSeconds += todoTime[2];
+            if (todo.status !== 'Skipped') {
+                if (typeof todo.time === "string") {
+                    let todoTime = todo.time.split(':').map((x) => +x);
+                    totalHours   += todoTime[0];
+                    totalMinutes += todoTime[1];
+                    totalSeconds += todoTime[2];
+                } else {
+                    totalHours += todo.time.getHours();
+                    totalMinutes += todo.time.getMinutes();
+                }
+            }
         }
         
         let timeCount = new Date();
@@ -107,13 +137,13 @@ taskcounter.controller("TodoListController", function($scope, $interval) {
     
     $scope.finalTime = function() {
         let currentDate = new Date();    
-        let totalTaskTime = $scope.totalTime();   
+        let totalsetTime = $scope.totalTime();   
 
         let finalTasksTime = new Date();
 
-        finalTasksTime.setHours(currentDate.getHours() + totalTaskTime.getHours());
-        finalTasksTime.setMinutes(currentDate.getMinutes() + totalTaskTime.getMinutes());
-        finalTasksTime.setSeconds(currentDate.getSeconds() + totalTaskTime.getSeconds());
+        finalTasksTime.setHours(currentDate.getHours() + totalsetTime.getHours());
+        finalTasksTime.setMinutes(currentDate.getMinutes() + totalsetTime.getMinutes());
+        finalTasksTime.setSeconds(currentDate.getSeconds() + totalsetTime.getSeconds());
        
         return finalTasksTime;
     }
